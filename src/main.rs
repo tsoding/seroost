c3d96c96e6b1e58662374b21b9ff6658bedcf0f8
rexim <reximkut@gmail.com>
2023-01-31 02:17:25 +0700

    Get rid of returns from entry()
    I just felt like doing it while browsing the code.

--- src/main.rs
+++ src/main.rs
@@ -124,7 +124,8 @@ fn entry() -> Result<(), ()> {
 
             let mut tf_index = TermFreqIndex::new();
             tf_index_of_folder(Path::new(&dir_path), &mut tf_index)?;
-            save_tf_index(&tf_index, "index.json")?;
+
+            save_tf_index(&tf_index, "index.json")
         },
         "search" => {
             let index_path = args.next().ok_or_else(|| {
@@ -148,6 +149,8 @@ fn entry() -> Result<(), ()> {
             for (path, rank) in search_query(&tf_index, &prompt).iter().take(20) {
                 println!("{path} {rank}", path = path.display());
             }
+
+            Ok(())
         }
         "serve" => {
             let index_path = args.next().ok_or_else(|| {
@@ -165,16 +168,14 @@ fn entry() -> Result<(), ()> {
 
             let address = args.next().unwrap_or("127.0.0.1:6969".to_string());
 
-            return server::start(&address, &tf_index)
+            server::start(&address, &tf_index)
         }
         _ => {
             usage(&program);
             eprintln!("ERROR: unknown subcommand {subcommand}");
-            return Err(());
+            Err(())
         }
     }
-
-    Ok(())
 }
 
 fn main() -> ExitCode {
