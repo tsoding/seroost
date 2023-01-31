e2ab6f5f6c77330ba4a6b5bb8690910dbcbd3455
rexim <reximkut@gmail.com>
2023-01-31 23:18:00 +0700

    Buffer IO to gain the speedz

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::env;
 use std::result::Result;
 use std::process::ExitCode;
 use std::str;
+use std::io::{BufReader, BufWriter};
 
 mod model;
 use model::*;
@@ -15,7 +16,7 @@ fn parse_entire_xml_file(file_path: &Path) -> Result<String, ()> {
     let file = File::open(file_path).map_err(|err| {
         eprintln!("ERROR: could not open file {file_path}: {err}", file_path = file_path.display());
     })?;
-    let er = EventReader::new(file);
+    let er = EventReader::new(BufReader::new(file));
     let mut content = String::new();
     for event in er.into_iter() {
         let event = event.map_err(|err| {
@@ -40,7 +41,7 @@ fn save_tf_index(tf_index: &TermFreqIndex, index_path: &str) -> Result<(), ()> {
         eprintln!("ERROR: could not create index file {index_path}: {err}");
     })?;
 
-    serde_json::to_writer(index_file, &tf_index).map_err(|err| {
+    serde_json::to_writer(BufWriter::new(index_file), &tf_index).map_err(|err| {
         eprintln!("ERROR: could not serialize index into file {index_path}: {err}");
     })?;
 
